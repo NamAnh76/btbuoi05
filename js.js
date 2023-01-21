9a0a190909726063e1f0c824582d540390c43a44
NamAnh76 <119741583+NamAnh76@users.noreply.github.com>
2023-01-21 22:33:14 +0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,28 @@
+setInterval(function(){
+    setTimeout(function(){
+        alert("Thương cho tấm thân cơ hàn, ngậm ngùi lặng nhìn con đò sang ngang");
+    }, 1000);
+    setTimeout(function(){
+        alert("Mộng vàng nay hoá tro tàn khi trên xe hoa ai đón đưa nàng");
+    }, 2000);
+    setTimeout(function(){
+        alert("Nhân duyên từ nay đứt đoạn, để lại một mình thân này héo hon");
+    }, 3000);
+    setTimeout(function(){
+        alert("Cuộc tình năm xưa chẳng còn, nhưng sao mãi giữ một tấm lòng son");
+    }, 4000);
+    setTimeout(function(){
+        alert("Hà cờ chi để ta phải xa cách rời, thiên ý trêu ngươi cho ta phận duyên đôi lứa đôi nơi");
+    }, 5000);
+}, 5000);
+
+
+localStorage.setItem('fullname','Phước')
+localStorage.setItem('fullname02','Hà')
+
+console.log(localStorage.getItem('fullname'));
+
+localStorage.removeItem('fullname')
+
+localStorage.clear();
+
